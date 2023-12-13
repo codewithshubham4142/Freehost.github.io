81e1a16d98426b824d8bba469f6b8bb24c7c0c83
codewithshubham4142 <153393272+codewithshubham4142@users.noreply.github.com>
2023-12-13 23:51:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+
+// Contact form
+function submitForm() {
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+
+  // Basic validation
+  if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+      alert('Please fill in all fields');
+      return;
+  }
+
+  // You can perform additional validation or send the form data to a server here
+
+  // For demonstration purposes, let's just display an alert
+  alert('Form submitted!\nName: ' + name + '\nEmail: ' + email + '\nMessage: ' + message);
+
+  // You can reset the form after submission if needed
+  document.getElementById('contactForm').reset();
+}
